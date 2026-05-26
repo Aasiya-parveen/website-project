368cd6d501e9a783afcb106d5c6dee9b91ea9e9b
unknown <rabiaahmed00111@gmail.com>
2026-05-26 17:25:18 +0500

    Added task manager UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,60 @@
 function App() {
   return (
-    <div>
-      <h1>TaskFlow Project</h1>
-      <p>Aasiya and Shoba Project</p>
+    <div style={{ padding: "20px" }}>
+
+      <h1>TaskFlow</h1>
+
+      <form>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Task Title"
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <textarea
+            placeholder="Enter Description"
+          ></textarea>
+        </div>
+
+        <br />
+
+        <div>
+          <select>
+            <option>Low</option>
+            <option>Medium</option>
+            <option>High</option>
+          </select>
+        </div>
+
+        <br />
+
+        <button>
+          Add Task
+        </button>
+
+      </form>
+
+      <hr />
+
+      <h2>Tasks</h2>
+
+      <div>
+
+        <h3>Learn React</h3>
+
+        <p>Practice frontend development</p>
+
+        <p>Priority: High</p>
+
+        <button>Delete</button>
+
+      </div>
+
     </div>
   );
 }
