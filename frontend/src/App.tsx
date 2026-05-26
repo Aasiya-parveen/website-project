8235eb7b0b1315564d6f53a224f27e41afcc35bd
unknown <rabiaahmed00111@gmail.com>
2026-05-26 17:16:04 +0500

    Created basic frontend layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,82 +1,9 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
-import { useState } from "react";
-
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-
-import TaskListPage from "./pages/TaskListPage";
-
-import CreateTaskPage from "./pages/CreateTaskPage";
-
-import { initialTasks } from "./data/mockTasks";
-
-import type { Task } from "./types/task";
-
 function App() {
-  const [tasks, setTasks] =
-    useState<Task[]>(initialTasks);
-
-  const [editingTask, setEditingTask] =
-    useState<Task | null>(null);
-
-  const handleAddTask = (task: Task) => {
-    if (editingTask) {
-      setTasks(
-        tasks.map((t) =>
-          t.id === task.id ? task : t
-        )
-      );
-
-      setEditingTask(null);
-    } else {
-      setTasks([...tasks, task]);
-    }
-  };
-
-  const handleDeleteTask = (id: number) => {
-    setTasks(
-      tasks.filter((task) => task.id !== id)
-    );
-  };
-
-  const handleEditTask = (task: Task) => {
-    setEditingTask(task);
-  };
-
   return (
-    <BrowserRouter>
-      <Header />
-
-      <Navbar />
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <TaskListPage
-              tasks={tasks}
-              onDelete={handleDeleteTask}
-              onEdit={handleEditTask}
-            />
-          }
-        />
-
-        <Route
-          path="/create"
-          element={
-            <CreateTaskPage
-              onAddTask={handleAddTask}
-              editingTask={editingTask}
-            />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>TaskFlow Project</h1>
+      <p>Aasiya and Shoba Project</p>
+    </div>
   );
 }
 
